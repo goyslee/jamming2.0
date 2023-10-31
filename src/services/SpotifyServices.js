12583dad3116ad2df51bd40536bf092fd9e165bb
goyslee <goyslee@gmail.com>
2023-10-31 16:58:16 +0000

    added code in to SpotifyServices.js to  use serverless code only if the environment is not local

--- src/services/SpotifyServices.js
+++ src/services/SpotifyServices.js
@@ -1,3 +1,4 @@
+//SpotifyServices.js
 import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
@@ -9,7 +10,9 @@ export const setAccessToken = (token) => {
   spotifyApi.setAccessToken(token);
 };
 
+
 export const getToken = async () => {
+
   const response = await fetch(process.env.REACT_APP_SPOTIFY_SERVICES_URL_1, {
     method: 'POST',
     headers: {
@@ -23,8 +26,12 @@ export const getToken = async () => {
   return data.access_token;
 };
 
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
 export const fetchTracks = async (query, accessToken) => {
-  try {
+if (isLocal) {
+    // Directly call the Spotify API or use mock data
+    try {
     spotifyApi.setAccessToken(accessToken);
     const response = await spotifyApi.searchTracks(query);
     console.log("Raw Spotify Response:", response); // Log the raw response
@@ -39,6 +46,34 @@ export const fetchTracks = async (query, accessToken) => {
     console.error("Error fetching tracks:", error);
     return []; // Return an empty array in case of an error
   }
+} else {
+    // Call the serverless function
+     try {
+    const response = await fetch("/.netlify/functions/spotify", {
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+      qs: {
+        path: `search?q=${query}&type=track`
+      }
+    });
+    const data = await response.json();
+    return data.tracks.items.map(track => ({
+      id: track.id,
+      name: track.name,
+      artist: track.artists[0].name,
+      album: track.album.name,
+      uri: track.uri
+    }));
+  } catch (error) {
+    console.error("Error fetching tracks:", error);
+    return [];
+  }
+}
+
+
+  
 };
 
 export const createPlaylist = async (userId, playlistName, accessToken) => {
