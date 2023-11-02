59f48cbc32f015edf3054dc8bac5c1ea044e3c3d
goyslee <goyslee@gmail.com>
2023-11-02 14:25:00 +0000

    serverless function authorisation complete change

--- netlify/functions/spotify.js
+++ netlify/functions/spotify.js
@@ -1,27 +1,33 @@
 //spotify.js
-import axios from 'axios';
+// In your Netlify function (e.g., spotify-token-exchange.js)
+const axios = require('axios');
 
-exports.handler = async function(event, context) {
-    const endpoint = "https://api.spotify.com/v1/"; // Spotify API endpoint
-    const path = event.queryStringParameters.path; // Get the desired path from query parameters
-    const token = event.headers.authorization; // Get the Spotify token from headers
+exports.handler = async (event) => {
+  const { code } = JSON.parse(event.body);
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
-    try {
-        const response = await axios.get(`${endpoint}${path}`, {
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json',
-               
-            }
-        });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(response.data),
-        };
-    } catch (error) {
-        return {
-            statusCode: error.response ? error.response.status : 500,
-            body: JSON.stringify(error.response ? error.response.data : {}),
-        };
-    }
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
+  params.append('redirect_uri', REDIRECT_URI);
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'),
+  };
+
+  try {
+    const response = await axios.post('https://accounts.spotify.com/api/token', params, { headers });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify(error.response.data),
+    };
+  }
 };
