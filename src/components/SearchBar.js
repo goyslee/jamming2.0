994b860f1c95c9180255d0cee67f0c0a7f8bfe47
goyslee <goyslee@gmail.com>
2023-11-03 20:06:28 +0000

    minor change in SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,15 @@
 // SearchBar.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function SearchBar({ onSearch }) {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (query) {
       try {
-          const results = await onSearch(query);
-          console.log("Search Results:", results);
+        const results = await onSearch(query);
+        console.log("Search Results:", results);
       } catch (error) {
         console.error("Error fetching tracks:", error);
         // Handle the error appropriately here, e.g., show an error message to the user
@@ -27,6 +27,8 @@ function SearchBar({ onSearch }) {
           onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit">Search</button>
+        <br></br>
+        <p>You can search by Artist, Album, or The name of the song.</p>
       </form>
     </div>
   );
